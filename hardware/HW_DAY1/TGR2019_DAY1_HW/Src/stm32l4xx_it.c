154362db341a6255fb0ac64886db2c2c652a4056
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-01-09 23:48:12 +0700

    Hardware homework firstday

---
+++ hardware/HW_DAY1/TGR2019_DAY1_HW/Src/stm32l4xx_it.c
@@ -0,0 +1,434 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32l4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+extern I2C_HandleTypeDef hI2cHandler;
+VL53L0X_Dev_t Dev =
+{
+  .I2cHandle = &hI2cHandler,
+  .I2cDevAddr = PROXIMITY_I2C_ADDRESS
+};
+ 
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+char rx_buff;
+float pressure_Value;
+float humidity_Value;
+float temp_Value;
+int16_t acc_value[3];
+int16_t mag_value[3];
+float gyro_value[3];
+uint16_t prox_value = 0;
+int init_done=1;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+static uint16_t VL53L0X_PROXIMITY_GetDistance(void);
+static void VL53L0X_PROXIMITY_MspInit(void);
+static void VL53L0X_PROXIMITY_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim6;
+extern UART_HandleTypeDef huart1;
+/* USER CODE BEGIN EV */
+uint8_t RX_BUFF[1];
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */ 
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Prefetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32L4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32l4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  //HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+	RX_BUFF[0] = USART1->RDR;
+	if(RX_BUFF[0] == '1'){
+		pressure_Value = BSP_PSENSOR_ReadPressure();
+		char str[80];
+		int pleum = sprintf(str,"Pressure = %4.2f\r\n",pressure_Value);
+		HAL_UART_Transmit(&huart1,str,pleum,5000);
+
+	}else if(RX_BUFF[0] == '2'){
+		humidity_Value = BSP_HSENSOR_ReadHumidity();
+		char hum[10];
+		gcvt(humidity_Value,4,hum);
+		char str[80];
+		strcpy(str, "HUMIDITY is = ");
+		strcat(str, hum);
+		strcat(str, "%\r\n");
+		HAL_UART_Transmit(&huart1,str,strlen(str),5000);
+
+	}else if(RX_BUFF[0] == '3'){
+		temp_Value = BSP_TSENSOR_ReadTemp();
+		char temp[10];
+		gcvt(temp_Value,4,temp);
+		char str[80];
+		strcpy(str, "TEMPERATURE is = ");
+		strcat(str, temp);
+		strcat(str, "C\r\n");
+		HAL_UART_Transmit(&huart1,str,strlen(str),5000);
+
+	}else if(RX_BUFF[0] == '4'){
+		BSP_ACCELERO_AccGetXYZ(acc_value);
+		char str[80];
+		int pleum = sprintf(str,"ACCELERO xyz = %d, %d, %d\r\n",acc_value[0],acc_value[1],acc_value[2]);
+		HAL_UART_Transmit(&huart1,str,pleum,5000);
+	}else if(RX_BUFF[0] == '5'){
+		BSP_GYRO_GetXYZ(gyro_value);
+		char str[80];
+		char gyro1[10];
+		char gyro2[10];
+		char gyro3[10];
+		gcvt(gyro_value[0],6,gyro1);
+		gcvt(gyro_value[1],6,gyro2);
+		gcvt(gyro_value[2],6,gyro3);
+		strcpy(str, "GYRO xyz = ");
+		strcat(str, gyro1);
+		strcat(str, ", ");
+		strcat(str, gyro2);
+		strcat(str, ", ");
+		strcat(str, gyro3);
+		strcat(str, "\r\n");
+		HAL_UART_Transmit(&huart1,str,strlen(str),5000);
+	}else if(RX_BUFF[0] == '6'){
+		BSP_MAGNETO_GetXYZ(mag_value);
+		char str[80];
+		int pleum = sprintf(str,"MAGNETOMETER xyz = %d, %d, %d\r\n",mag_value[0],mag_value[1],mag_value[2]);
+		HAL_UART_Transmit(&huart1,str,pleum,5000);
+	}else if(RX_BUFF[0] == '7'){
+		if(init_done = 1){
+			VL53L0X_PROXIMITY_Init();
+			init_done=2;
+		}
+		prox_value = VL53L0X_PROXIMITY_GetDistance();
+		char str[80];
+		int pleum = sprintf(str,"Proximity = %d\r\n",prox_value);
+		HAL_UART_Transmit(&huart1,str,pleum,5000);
+	}else{
+		HAL_UART_Transmit(&huart1,"no sensor\r\n",11,5000);
+	}
+  /* USER CODE END USART1_IRQn 1 */
+}
+static uint16_t VL53L0X_PROXIMITY_GetDistance(void)
+{
+  VL53L0X_RangingMeasurementData_t RangingMeasurementData;
+
+  VL53L0X_PerformSingleRangingMeasurement(&Dev, &RangingMeasurementData);
+
+  return RangingMeasurementData.RangeMilliMeter;
+}
+static void VL53L0X_PROXIMITY_Init(void)
+{
+  uint16_t vl53l0x_id = 0;
+  VL53L0X_DeviceInfo_t VL53L0X_DeviceInfo;
+
+  /* Initialize IO interface */
+  SENSOR_IO_Init();
+  VL53L0X_PROXIMITY_MspInit();
+
+  memset(&VL53L0X_DeviceInfo, 0, sizeof(VL53L0X_DeviceInfo_t));
+
+  if (VL53L0X_ERROR_NONE == VL53L0X_GetDeviceInfo(&Dev, &VL53L0X_DeviceInfo))
+  {
+    if (VL53L0X_ERROR_NONE == VL53L0X_RdWord(&Dev, VL53L0X_REG_IDENTIFICATION_MODEL_ID, (uint16_t *) &vl53l0x_id))
+    {
+      if (vl53l0x_id == VL53L0X_ID)
+      {
+        if (VL53L0X_ERROR_NONE == VL53L0X_DataInit(&Dev))
+        {
+          Dev.Present = 1;
+          SetupSingleShot(Dev);
+        }
+        else
+        {
+          printf("VL53L0X Time of Flight Failed to send its ID!\n");
+        }
+      }
+    }
+    else
+    {
+      printf("VL53L0X Time of Flight Failed to Initialize!\n");
+    }
+  }
+  else
+  {
+    printf("VL53L0X Time of Flight Failed to get infos!\n");
+  }
+}
+
+
+/**
+  * @brief  VL53L0X proximity sensor Msp Initialization.
+  */
+static void VL53L0X_PROXIMITY_MspInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /*Configure GPIO pin : VL53L0X_XSHUT_Pin */
+  GPIO_InitStruct.Pin = VL53L0X_XSHUT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(VL53L0X_XSHUT_GPIO_Port, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(VL53L0X_XSHUT_GPIO_Port, VL53L0X_XSHUT_Pin, GPIO_PIN_SET);
+
+  HAL_Delay(1000);
+}
+
+
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_10);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_14);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_15);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM6 global interrupt, DAC channel1 and channel2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
